308febc5481801232d532ef427115023335f9727
Francisco Soares <fmssn@cin.ufpe.br>
2024-05-07 16:10:42 -0300

    adiciona arquivos do projeto

---
+++ CaoculadoraTarde/CaoculadoraTarde/CaoculadoraTardeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoculadoraTardeApp.swift
+//  CaoculadoraTarde
+//
+//  Created by Francisco Miranda Soares on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoculadoraTardeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
