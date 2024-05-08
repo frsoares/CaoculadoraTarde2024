d223dc5264c343cb6fda81f3eed80399eab75036
Francisco Soares <fmssn@cin.ufpe.br>
2024-05-08 18:03:52 -0300

    Adiciona imagem de clarinha e implementa inicialmente o resultado

--- CaoculadoraTarde/CaoculadoraTarde/ContentView.swift
+++ CaoculadoraTarde/CaoculadoraTarde/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
 
     @State var years: Int? = nil
     @State var months: Int? = nil
+    @State var result: Int?
 
     var body: some View {
         VStack(alignment: .leading) {
@@ -28,6 +29,30 @@ struct ContentView: View {
                 value: $months,
                 format: .number
             )
+            Text("Porte")
+            // aqui vai ficar o segmented control
+            if let result {
+                Text("Seu cachorro tem, em idade humana...")
+                Text("\(result) anos")
+            } else {
+                Image(ImageResource.clarinha)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 150)
+                    .frame(maxWidth: .infinity)
+                    .shadow(radius: 20)
+            }
+            Button(action: {
+                result = 23
+            }, label: {
+                ZStack {
+                    Color.purple
+                    Text("Cãocular")
+                        .foregroundStyle(.white)
+                }
+            })
+            .cornerRadius(10)
+            .frame(height: 50)
         }
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
