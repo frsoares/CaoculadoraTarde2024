f0af71a74b339cb92f334a54ec63203ba2e4c718
Francisco Soares <fmssn@cin.ufpe.br>
2024-05-13 15:21:52 -0300

    Implementa cores, navigationbar e scroll, e ajusta layout.

--- CaoculadoraTarde/CaoculadoraTarde/ContentView.swift
+++ CaoculadoraTarde/CaoculadoraTarde/ContentView.swift
@@ -15,69 +15,89 @@ struct ContentView: View {
     @State var porteSelecionado: Porte = .pequeno
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Qual a idade do seu cão?")
-                .font(.header5)
-            Text("Anos")
-                .font(.body1)
-            TextField(
-                "Quantos anos completos seu cão tem.",
-                value: $years,
-                format: .number
-            )
-            Text("Meses")
-                .font(.body1)
-            TextField(
-                "E quantos meses além disso ele tem.",
-                value: $months,
-                format: .number
-            )
-            Text("Porte")
-                .font(.body1)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20.0) {
+                    Text("Qual a idade do seu cão?")
+                        .font(.header5)
+                        .padding(.top, 24)
+                    VStack(alignment: .leading, spacing: 8.0) {
+                        Text("Anos")
+                            .font(.body1)
+                        TextField(
+                            "Quantos anos completos seu cão tem.",
+                            value: $years,
+                            format: .number
+                        )
+                    }
+                    VStack(alignment: .leading, spacing: 8.0) {
 
-            Picker("Porte", selection: $porteSelecionado) {
-                ForEach(Porte.allCases, id: \.self) { porte in
-                    Text(porte.rawValue)
-                        .tag(porte)
+                        Text("Meses")
+                            .font(.body1)
+                        TextField(
+                            "E quantos meses além disso ele tem.",
+                            value: $months,
+                            format: .number
+                        )
+                    }
+                    VStack(alignment: .leading, spacing: 8.0) {
+
+                        Text("Porte")
+                            .font(.body1)
+
+                        Picker("Porte", selection: $porteSelecionado) {
+                            ForEach(Porte.allCases, id: \.self) { porte in
+                                Text(porte.rawValue)
+                                    .tag(porte)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    Divider()
+                        .background(.purple)
+                    Spacer()
+
+                    if let result {
+                        Text("Seu cachorro tem, em idade humana...")
+                            .font(.body1)
+                            .frame(maxWidth: .infinity)
+                        Text("\(result) anos")
+                            .font(.display)
+                            .frame(maxWidth: .infinity)
+                    } else {
+                        Image(ImageResource.clarinha)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxHeight: 150)
+                            .frame(maxWidth: .infinity)
+                            .shadow(radius: 20)
+                    }
+
+                    Spacer()
+                    Button(action: processYears, label: {
+                        ZStack {
+                            Color.indigo600
+                            Text("Cãocular")
+                                .foregroundStyle(.white)
+                                .font(.body1)
+                        }
+                    })
+                    .cornerRadius(10)
+                    .frame(height: 50)
                 }
+                .containerRelativeFrame(.vertical)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .bold()
+                .fontDesign(.rounded)
+                .padding()
+                .navigationTitle("Cãoculadora")
+                .toolbarBackground(.visible, for: .navigationBar)
+                .toolbarBackground(.indigo600, for: .navigationBar)
+                .toolbarColorScheme(.dark, for: .navigationBar, .tabBar)
             }
-            .pickerStyle(.segmented)
-
-            Divider()
-                .background(.purple)
-            Spacer()
-
-            if let result {
-                Text("Seu cachorro tem, em idade humana...")
-                    .font(.body1)
-                Text("\(result) anos")
-                    .font(.display)
-            } else {
-                Image(ImageResource.clarinha)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 150)
-                    .frame(maxWidth: .infinity)
-                    .shadow(radius: 20)
-            }
-
-            Spacer()
-            Button(action: processYears, label: {
-                ZStack {
-                    Color.purple
-                    Text("Cãocular")
-                        .foregroundStyle(.white)
-                        .font(.body1)
-                }
-            })
-            .cornerRadius(10)
-            .frame(height: 50)
+            .scrollDismissesKeyboard(.immediately)
         }
-        .textFieldStyle(.roundedBorder)
-        .keyboardType(.numberPad)
-        .bold()
-        .fontDesign(.rounded)
-        .padding()
     }
 
     func processYears() {
