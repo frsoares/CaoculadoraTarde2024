9de9a1b365cd297b580a95df708e083fd2e255a9
andressaluna <luna@MacBook-Pro-63.local>
2024-06-17 17:14:59 -0300

    fix values

--- CaoculadoraTarde/CaoculadoraTarde/Porte.swift
+++ CaoculadoraTarde/CaoculadoraTarde/Porte.swift
@@ -17,11 +17,11 @@ enum Porte: String, CaseIterable {
         let multiplicador: Int
         switch self {
         case .small:
-            multiplicador = 6
+            multiplicador = 1
         case .medium:
-            multiplicador = 7
+            multiplicador = 2
         case .big:
-            multiplicador = 8
+            multiplicador = 3
         }
 
         result = anos * multiplicador + meses * multiplicador / 24
