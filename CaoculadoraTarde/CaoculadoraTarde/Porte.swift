f40255b2adc32747d4196ac1551c329af47d0b11
Mateus Gonçalves <mateusg260@gmail.com>
2024-06-17 17:24:03 -0300

    Refacts enum porte

--- CaoculadoraTarde/CaoculadoraTarde/Porte.swift
+++ CaoculadoraTarde/CaoculadoraTarde/Porte.swift
@@ -12,16 +12,20 @@ enum Porte: String, CaseIterable {
     case medium = "Médio"
     case large = "Grande"
 
-    func conversaoDeIdade(anos: Int, meses: Int) -> Int {
+    func conversaoDeIdade(
+        anos: Int,
+        meses: Int
+    ) -> Int {
         let result: Int
         let multiplicador: Int
+        
         switch self {
         case .small:
-            multiplicador = 1
+            multiplicador = 10
         case .medium:
-            multiplicador = 2
+            multiplicador = 20
         case .large:
-            multiplicador = 3
+            multiplicador = 30
         }
 
 
